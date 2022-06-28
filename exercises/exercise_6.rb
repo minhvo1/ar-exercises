6861722e550cc6367ebd70401854d0cf7f054e61
Minh Vo <minhvo199@gmail.com>
2022-06-28 00:41:15 +0000

    complete ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create([
+  {first_name: "Minh", last_name: "Vo", hourly_rate: 15},
+  {first_name: "First", last_name: "Last", hourly_rate: 20},
+])
+
+@store2.employees.create([
+  {first_name: "Em", last_name: "Ployee", hourly_rate: 25},
+  {first_name: "New", last_name: "Person", hourly_rate: 30},
+])
